f712ee6f37fabdb5881978baf70c5aa8b8387f9d
Crowdbotics <team@crowdbotics.com>
2024-06-16 08:44:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-limit-48451",
+  url: "https://purple-limit-48451.botics.co",
+  api: "https://purple-limit-48451.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
